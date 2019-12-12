7d8404b328734e58a313207e1b0a1621d3e5151e
Raj kothari <rkmeansrk39@gmail.com>
2019-12-12 15:02:00 +0530

    update

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+ @Output() sendDetailOfRecipe = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Test name',
     'test Description about the food',
     'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiK6ZqHnanmAhXcyjgGHajvBioQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fbeauty%2F&psig=AOvVaw0Ts4sWi4lKHGt4mC2qhtrt&ust=1576003590427231'
-    )
+    ),
+    new Recipe('Test name 2',
+    'Test Description of second item of the food',
+    'link of the image')
   ];
   constructor() { }
-
+  fillDetail(recipe : Recipe){
+    console.log("ready to send Detail by senddetailofrecipe");
+    this.sendDetailOfRecipe.emit(recipe);
+  }
   ngOnInit() {
   }
 
