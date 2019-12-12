f98f96fd7bff80c99e888264f5a553f84b7c6f15
Raj kothari <rkmeansrk39@gmail.com>
2019-12-12 14:55:44 +0530

    update

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import {Recipe} from '../../recipe.model';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-
+  @Input() recipe : Recipe;
+ @Output() detailEmitter = new EventEmitter<void>();
   constructor() { }
-
   ngOnInit() {
   }
-
+  showDetail(){
+    console.log("detail emitter in recipe item click is working");
+    this.detailEmitter.emit();
+  }
 }
