e8a3d3b0990d69315c5ffbb78f964e2ba4757452
Raj kothari <rkmeansrk39@gmail.com>
2019-12-22 11:47:37 +0530

    update

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,10 +10,18 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() detailRecipe : Recipe;
-  constructor() { }
+  constructor(private recipeService : RecipeService) { }
 
   ngOnInit() {
   }
-
+  dropdownState : boolean = false;
+  changedropdownState(){
+  this.dropdownState = !this.dropdownState;
+  console.log("working click");
+  console.log(this.dropdownState);
+  }
+  onAddToShoppingList(){
+this.recipeService.addIngredientToShoppingList(this.detailRecipe.ingredients);
+  }
 
 }
