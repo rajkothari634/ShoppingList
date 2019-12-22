ceaa1d375cd62f1cfbdb4b3b99fe1f67a215c14e
Raj kothari <rkmeansrk39@gmail.com>
2019-12-23 05:14:02 +0530

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,10 @@
-import { Component,EventEmitter, Output } from "@angular/core";
+import { Component} from "@angular/core";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent{
-@Output() pagedecider = new EventEmitter<string>();
-openPage(page:string){
-    this.pagedecider.emit(page);
-}
 dropdownState : boolean = false;
 changedropdownState(){
 this.dropdownState = !this.dropdownState;
