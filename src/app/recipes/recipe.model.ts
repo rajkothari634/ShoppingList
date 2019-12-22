e9a6580a545875504fa9b3a46765412b6a8f0f47
Raj kothari <rkmeansrk39@gmail.com>
2019-12-22 11:48:03 +0530

    update

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,12 +1,16 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Recipe{
     public name: string;
     public discription: string;
     public imagePath: string;
+    public ingredients : Ingredient[];
     
-    constructor(name: string,desc: string,imagePath: string)
+    constructor(name: string,desc: string,imagePath: string,ingredients:Ingredient[])
     {
         this.name = name;
         this.discription = desc;
         this.imagePath= imagePath;
+        this.ingredients = ingredients;
     }    
 }
