cc60944af3973033b5ab607280a2d13e38edb4a7
Raj kothari <rkmeansrk39@gmail.com>
2019-12-23 05:11:08 +0530

    update

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,26 +9,27 @@ export class RecipeService{
     private recipes: Recipe[] = [
         new Recipe('Test name',
         'test Description about the food',
-        'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=416&h=312&q=60'
-        ,
+        'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=416&h=312&q=60',
         [
             new Ingredient("banana",30),
             new Ingredient("milk (in litre)", 1),
             new Ingredient('sugar (in gram)',50)
-        ]),
+        ],1),
         new Recipe('Test name 2',
         'Test Description of second item of the food',
-        'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=416&h=312&q=60'
-        ,
+        'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=416&h=312&q=60',
         [
             new Ingredient("banana",60),
             new Ingredient("milk (in litre)", 2),
             new Ingredient('sugar (in gram)',100)
-        ])
+        ],2)
     ];
     constructor(private shoppingListService : ShoppingListServices){}
     addIngredientToShoppingList(ingredients : Ingredient[]){
         this.shoppingListService.addIngredients(ingredients);
+    }
+    getRecipeById(id:number){
+        return this.recipes[id-1];
     }
       getRecipes(){
           return this.recipes.slice();
