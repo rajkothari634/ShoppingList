927c5180092944004da21bff2d431a58329de506
Raj kothari <rkmeansrk39@gmail.com>
2019-12-23 05:08:44 +0530

    update

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Recipe} from '../../recipe.model';
 import { RecipeService } from '../../recipe.service';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
@@ -9,11 +10,15 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
   @Input() recipe : Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  //constructor(private recipeService: RecipeService) { }
+  constructor(private router: Router,
+              private route: ActivatedRoute){}
   ngOnInit() { 
   }
-  showDetail(){
-    console.log("detail emitter in recipe item click is working");
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
+  // showDetail(id: number){
+  //   console.log(id);
+  //   // console.log("detail emitter in recipe item click is working");
+  //   // this.recipeService.recipeSelected.emit(this.recipe);
+  //   this.router.navigate([id],{relativeTo: this.route});
+  // }
 }
