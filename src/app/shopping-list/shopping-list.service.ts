dae5dfe996c1b5b13857a0a703937636dffaa0a8
Raj kothari <rkmeansrk39@gmail.com>
2019-12-24 18:40:50 +0530

    update

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
 import { Ingredient } from '../shared/ingredient.model';
-import { EventEmitter } from '@angular/core';
+import {Subject} from 'rxjs';
 
 export class ShoppingListServices{
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
    private ingredients: Ingredient[] = [
         new Ingredient('Apple',5),
         new Ingredient('orange',10)
@@ -12,10 +12,10 @@ export class ShoppingListServices{
       }
       addIngredient(ingredient : Ingredient){
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
     addIngredients(ingredients : Ingredient[]){
         this.ingredients.push(...ingredients);
-        this.ingredientsChanged.emit(this.ingredients);
+        this.ingredientsChanged.next(this.ingredients);
     }
 }
