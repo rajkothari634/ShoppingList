2e3484b6b5ddaa8aeb74c3fed77993c9143f5089
Raj kothari <rkmeansrk39@gmail.com>
2019-12-24 18:40:14 +0530

    update

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListServices } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit,OnDestroy {
   ingredients: Ingredient[];
+  private igchangedSub : Subscription
 
   constructor( private shoppinglistService :ShoppingListServices) {}
 
+  ngOnDestroy() : void {
+    this.igchangedSub.unsubscribe();
+  }
   ngOnInit() {
     this.ingredients=this.shoppinglistService.getIngredients();
-    this.shoppinglistService.ingredientsChanged
+    this.igchangedSub = this.shoppinglistService.ingredientsChanged
     .subscribe(
       (newingredients: Ingredient[])=>{
         this.ingredients = newingredients;
