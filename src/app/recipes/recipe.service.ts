eb32e23088d0fb08fb67dc258a2f9ab0aac1c054
Raj kothari <rkmeansrk39@gmail.com>
2019-12-24 18:37:58 +0530

    update

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,10 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListServices } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService{
-    recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
         new Recipe('Test name',
         'test Description about the food',
