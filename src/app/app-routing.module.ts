6b143953c7b50bac3ae5cc3cffe699c71501aa78
Raj kothari <rkmeansrk39@gmail.com>
2019-12-29 03:18:47 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeResolverService } from './recipes/recipes-resolver.sercive';
 
 const appRoutes : Routes = [
     { path: '',redirectTo: '/recipes',pathMatch: 'full'},
     { path: 'recipes',component: RecipesComponent , children:[
         {path:"",component:RecipeStartComponent},
         {path: "new", component: RecipeEditComponent},
-        {path : ':id',component: RecipeDetailComponent},
-        {path: ":id/edit",component: RecipeEditComponent}
+        {path : ':id',component: RecipeDetailComponent, resolve : [RecipeResolverService]},
+        {path: ":id/edit",component: RecipeEditComponent, resolve : [RecipeResolverService]}
       ]},
     { path: 'shoppinglist',component: ShoppingListComponent}
 ]
