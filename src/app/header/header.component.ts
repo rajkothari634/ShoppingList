e34746db791ae8b76beca590f3536f9f9d43bed7
Raj kothari <rkmeansrk39@gmail.com>
2019-12-29 03:22:23 +0530

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component} from "@angular/core";
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent{
-dropdownState : boolean = false;
-changedropdownState(){
-this.dropdownState = !this.dropdownState;
-}
+    dropdownState : boolean = false;
+    changedropdownState(){
+        this.dropdownState = !this.dropdownState;
+    }
+    constructor(private dataStorageService : DataStorageService){}
+    onSaveData(){
+        this.dataStorageService.storeRecipes();
+    }
+    onFetchData(){
+        this.dataStorageService.fetchRecipes().subscribe();;
+    }
 }
