8d6bab5d8446073f90684bd82cc7277c35902901
Raj kothari <rkmeansrk39@gmail.com>
2019-12-29 20:37:41 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,21 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeResolverService } from './recipes/recipes-resolver.sercive';
+import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const appRoutes : Routes = [
     { path: '',redirectTo: '/recipes',pathMatch: 'full'},
-    { path: 'recipes',component: RecipesComponent , children:[
+    { path: 'recipes',component: RecipesComponent ,
+    canActivate: [AuthGuard],
+     children:[
         {path:"",component:RecipeStartComponent},
         {path: "new", component: RecipeEditComponent},
         {path : ':id',component: RecipeDetailComponent, resolve : [RecipeResolverService]},
         {path: ":id/edit",component: RecipeEditComponent, resolve : [RecipeResolverService]}
       ]},
-    { path: 'shoppinglist',component: ShoppingListComponent}
+    { path: 'shoppinglist',component: ShoppingListComponent},
+    {path: 'auth',component : AuthComponent}
 ]
 @NgModule({
     imports:[RouterModule.forRoot(appRoutes)],
