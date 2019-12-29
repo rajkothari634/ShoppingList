ac1197ac21850287eeb579c48a5f64e352306132
Raj kothari <rkmeansrk39@gmail.com>
2019-12-29 20:38:01 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ShopProject';
   active = "Recipe";
   onNavigate(activenow:string){
     this.active=activenow;
+  }
+  constructor(private authService : AuthService){
+
+  }
+  ngOnInit(){
+    this.authService.autoLogin(); 
   }
 }
