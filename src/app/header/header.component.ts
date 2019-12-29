bfbff8cdc07e9d6befcbf8e3cc1093a57d922d49
Raj kothari <rkmeansrk39@gmail.com>
2019-12-29 20:39:41 +0530

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,40 @@
-import { Component} from "@angular/core";
+import { Component, OnInit, OnDestroy} from "@angular/core";
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent{
+export class HeaderComponent implements OnInit,OnDestroy{
     dropdownState : boolean = false;
+    userSub : Subscription;
+    isAuth = false;
     changedropdownState(){
         this.dropdownState = !this.dropdownState;
     }
-    constructor(private dataStorageService : DataStorageService){}
+    constructor(private dataStorageService : DataStorageService
+        ,private authServices : AuthService,
+        private router : Router){}
     onSaveData(){
         this.dataStorageService.storeRecipes();
     }
     onFetchData(){
         this.dataStorageService.fetchRecipes().subscribe();;
+    }
+    logout(){
+        this.authServices.logout();
+        this.router.navigate(['/auth']);
+    }
+    ngOnInit(){
+        this.userSub = this.authServices.user.subscribe(user => {
+            this.isAuth = !!user;
+            console.log(!!user);
+        });
+    }
+    ngOnDestroy(){
+        this.userSub.unsubscribe();
     }
 }
